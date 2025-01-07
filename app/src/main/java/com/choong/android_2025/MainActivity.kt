1c0561babbcd7d09de195fbac79b49f3b3e48217
LeeChoongHwan <choo901@naver.com>
2025-01-07 16:41:59 +0900

    set intentLogin

--- app/src/main/java/com/choong/android_2025/MainActivity.kt
+++ app/src/main/java/com/choong/android_2025/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.choong.android_2025
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.choong.android_2025.databinding.ActivityMainBinding
@@ -7,9 +8,14 @@ import com.choong.android_2025.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding : ActivityMainBinding
+    private val intentLogin= Intent(this, LoginActivity::class.java)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.btnLogin.setOnClickListener {
+            startActivity(intentLogin)
+        }
     }
 }
